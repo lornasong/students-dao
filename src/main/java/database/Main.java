09b13792d45a2e94b83ed1c4b4db324107caafde
Lorna Song <lorna.song@gmail.com>
2015-02-26 22:02:40 -0500

    Met with Gray
     -  CSS files working now

--- src/main/java/database/Main.java
+++ src/main/java/database/Main.java
@@ -1,21 +1,27 @@
 package database;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.HandlerCollection;
 import org.eclipse.jetty.server.nio.SelectChannelConnector;
 
+import com.j256.simplewebframework.displayer.FileResultDisplayer;
+import com.j256.simplewebframework.displayer.JsonResultDisplayer;
+import com.j256.simplewebframework.displayer.ResultDisplayer;
 import com.j256.simplewebframework.displayer.StringResultDisplayer;
 import com.j256.simplewebframework.freemarker.FreemarkerHtmlDisplayer;
 import com.j256.simplewebframework.handler.LoggingHandler;
 import com.j256.simplewebframework.handler.ServiceHandler;
 import com.j256.simplewebframework.resource.FileLocator;
+import com.j256.simplewebframework.resource.LocalResourceHandler;
 
 import freemarker.template.Configuration;
 
 public class Main {
-	
+
 	private static final int DEFAULT_WEB_PORT = 8080;
 
 	public static void main(String[] args) throws Exception {
@@ -30,35 +36,49 @@ public class Main {
 
 		DaoUtil daoMain = new DaoUtil();
 		daoMain.configure();
-		
+
 		// create a service handler
 		ServiceHandler serviceHandler = new ServiceHandler();
-				
+
 		// register our service that handles requests from simple-web-framework
 		serviceHandler.registerWebService(new HomeService());
-		serviceHandler.registerWebService(new ModifyService(daoMain.getStudentDao()));
-		serviceHandler.registerWebService(new ViewService(daoMain.getStudentDao()));
-		
+		serviceHandler.registerWebService(new ModifyService(daoMain
+				.getStudentDao()));
+		serviceHandler.registerWebService(new ViewService(daoMain
+				.getStudentDao()));
+
 		// register a displayer of String results
 		serviceHandler.registerResultDisplayer(new StringResultDisplayer());
-		
-		//Stuff for to use Freemarker instead of stringbuilding html
-		//Reminder: Has errors. Need to press forward twice. Page will render.
-		FreemarkerHtmlDisplayer displayer = new FreemarkerHtmlDisplayer();
-		FileLocator fileLocator = new FileLocator(new File("target/classes"), new String [] { "index.html" });
-		displayer.setFileLocator(fileLocator);
+
+		// Stuff for to use Freemarker instead of stringbuilding html
+		// Reminder: Has errors. Need to press forward twice. Page will render.
+		FreemarkerHtmlDisplayer htmlDisplayer = new FreemarkerHtmlDisplayer();
+		FileLocator fileLocator = new FileLocator(new File("target/classes"),
+				new String[] { "index.html" });
+		htmlDisplayer.setFileLocator(fileLocator);
 		Configuration configuration = new Configuration();
-		displayer.setTemplateConfig(configuration);
-		serviceHandler.registerResultDisplayer(displayer);
+		htmlDisplayer.setTemplateConfig(configuration);
+		serviceHandler.registerResultDisplayer(htmlDisplayer);
+		serviceHandler.registerResultDisplayer(new JsonResultDisplayer());
 		
-		//Handlers
+		// This handler displays css files, jpegs, etc that don't need to be
+		// processed by FreeMarker
+		LocalResourceHandler localResourceHandler = new LocalResourceHandler();
+		localResourceHandler.setFileLocator(fileLocator);
+		localResourceHandler.setDefaultDisplayer(new FileResultDisplayer());
+		Map<String, ResultDisplayer> extMap = new HashMap<String, ResultDisplayer>();
+		extMap.put("html", htmlDisplayer);
+
+		// Handlers
 		HandlerCollection handlers = new HandlerCollection();
 		handlers.addHandler(serviceHandler);
-		handlers.addHandler(new WrongUrlHandler());
+		handlers.addHandler(localResourceHandler);
 		
+		handlers.addHandler(new WrongUrlHandler());
+
 		LoggingHandler loggingHandler = new LoggingHandler();
 		loggingHandler.setHandler(handlers);
-		
+
 		server.setHandler(loggingHandler);
 		server.start();
 	}
